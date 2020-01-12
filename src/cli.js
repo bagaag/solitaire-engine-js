195e2f7644a6927070134f9668eb9d559d8dc57e
Matt Wiseley <wiseley@gmail.com>
2020-01-12 09:33:37 -0500

    move validation

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,13 @@
 // provides a command line ux for the game engine
 
-let games = require('./game.js');
+const games = require('./game.js');
 let game = new games.Game();
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-let pr = console.log;
+const pr = console.log;
 
 // input loop
 let inputLoop = function () {
@@ -27,8 +27,8 @@ let inputLoop = function () {
     else if (ans == 'w') {
       waste();
     }
-    else if (ans == 'm') {
-      move(args(ans));
+    else if (ans.startsWith('m')) {
+      move(ans);
     }
     else {
       help();
@@ -93,7 +93,43 @@ function help() {
   pr("s: show active card and stoc count");
   pr("w: show waste count");
   pr("n: new game");
-  pr("m <from: s|f1-f4|t1-t7> <to: f1-f4|t1-t7>: moves a card from stock, a foundation or tableau to a foundation or tableau");
+  pr("m [from: s|f1-f4|t1-t7 to: f1-f4|t1-t7]]: moves a card from stock, a foundation or tableau to a foundation or tableau");
   pr("p: pass, move active card from stock to waste");
   pr("x: exit");
+}
+
+const moveRE = /m\s+([stf])([1-7])?\s+([tf])([1-7])/;
+
+function move(args) {
+  if (args == 'm') {
+    moveAuto();
+  }
+  else {
+    // parse arguments
+    let m = args.match(moveRE);
+    let from = m[1];
+    let fromIx = m[2];
+    let to = m[3];
+    let toIx = m[4];
+      
+    // validate arguments
+    if (!game.canMove(from, fromIx, to, toIx)) {
+      pr('Invalid move.');
+        return;
+    }
+
+    // move the card
+    game.move(from, fromIx, to, toIx);
+
+    // display result
+    if (from =='t' || to == 't') {
+      tableau();
+    }
+    if (from == 'f' || to == 'f') {
+      foundations();
+    }
+    if (from == 's') {
+      stock();
+    }
+  }
 }
--- src/game.js
+++ src/game.js
@@ -34,6 +34,104 @@ class Game {
     return arr;
   }
 
+  // checks input values and move feasability
+  canMove(from, fromIx, to, toIx) {
+    // check 'to' values
+    if (to == 'f') {
+      if (toIx < 1 || toIx > 4) {
+        return false;
+      }
+    }
+    else if (to == 't') {
+      if (toIx < 1 || toIx > 7) {
+        return false;
+      }
+    }
+    else {
+      return false;
+    }
+
+    // check 'from' values
+    if (from == 'f') {
+      if (fromIx < 1 || fromIx > 4) {
+        return false;
+      }
+    }
+    else if (from == 't') {
+      if (fromIx < 1 || fromIx > 7) {
+        return false;
+      }
+    }
+    else if (from != 's') {
+      return false;
+    }
+    
+    // get card to move
+    let card = this.peek(from, fromIx);
+    if (!card) {
+      return false;
+    }
+
+    // test move feasability
+    let dest = this.peek(to, toIx);
+    if (to == 't') {
+      if (!dest) {
+        // first tableau card must be a king
+        if (card.rank != 13) {
+          return false;
+        }
+      }
+      else {
+        // must alternate red/black suits
+        if (card.suit % 2 == dest.suit % 2) {
+          return false;
+        }
+        // must be one rank smaller than the parent card
+        if (card.rank != dest.rank - 1) {
+          return false;
+        }
+      }
+    }
+    else {
+      // first card must be an ace
+      if (!dest) {
+        if (card.rank != 1) {
+          return false;
+        }
+      }
+      else {
+        // must be same suit and one rank higher than dest
+        if (card.suit != dest.suit || card.rank != dest.rank + 1) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  // returns a card from (s)tock, (t)ableau or (f)oundation without removing from its position
+  peek(from, fromIx) {
+    if (from == 's') {
+      return this.stock.last();
+    }
+    if (from == 't') {
+      let t = this.tableau[fromIx];
+      if (t) {
+        return t.last();
+      }
+    }
+    if (from == 'f') {
+      let f = this.foundations[fromIx];
+      if (f) {
+        return f.last();
+      }
+    }
+  }
+
+  move(from, fromIx, to, toIx) {
+    console.log('move...');
+  }
+
 }
 
 module.exports = {
